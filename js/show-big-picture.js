2e0ac6e04fb339dc5df1ff5750225565654ff285
FrenzyPet <manpatbro@gmail.com>
2022-11-01 18:24:22 +0300

    выполнение ДЗ 7.2

--- js/show-big-picture.js
+++ js/show-big-picture.js
@@ -1,39 +1,60 @@
-const modalBigPicture = document.querySelector('.big-picture');
-const otherUsersPhotos = document.querySelectorAll('.picture');
-const closeButton = modalBigPicture.querySelector('.big-picture__cancel');
-const loadCommentButton = modalBigPicture.querySelector('.comments-loader');
-const commentCounter = modalBigPicture.querySelector('.social__comment-count');
+const hideModal = (pictureNode) => {
+  pictureNode.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+};
 
-const openBigPicture = () => {
-  modalBigPicture.classList.remove('hidden');
-  document.body.classList.add('modal-open');
-  commentCounter.classList.add('hidden');
-  loadCommentButton.classList.add('hidden');
-
-  closeButton.addEventListener('click', () => {
-    modalBigPicture.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-  });
-  document.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Escape') {
-      modalBigPicture.classList.add('hidden');
-      document.body.classList.remove('modal-open');
+const showBigPicture = (similarPhotos) => {
+  const modalBigPicture = document.querySelector('.big-picture');
+  const otherUsersPhotos = document.querySelectorAll('.picture');
+  const closeButton = modalBigPicture.querySelector('.big-picture__cancel');
+  const loadCommentButton = modalBigPicture.querySelector('.comments-loader');
+  const commentCounter = modalBigPicture.querySelector('.social__comment-count');
+  const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+  const commentsList = document.querySelector('.social__comments');
+  const openBigPicture = () => {
+    const hideModalByClick = () => {
+      hideModal(modalBigPicture);
+      closeButton.removeEventListener('click', hideModalByClick);
+      document.removeEventListener('keydown', hideModalByEscape);
+    };
+    function hideModalByEscape(evt) {
+      if (evt.key === 'Escape') {
+        hideModal(modalBigPicture);
+      }
+      closeButton.removeEventListener('click', hideModalByClick);
+      document.removeEventListener('keydown', hideModalByEscape);
     }
-  });
+    commentsList.innerHTML = ' ';
+    modalBigPicture.classList.remove('hidden');
+    document.body.classList.add('modal-open');
+    commentCounter.classList.add('hidden');
+    loadCommentButton.classList.add('hidden');
+    closeButton.addEventListener('click', hideModalByClick);
+    document.addEventListener('keydown', hideModalByEscape);
+  };
+  const addPhotoContent = (photo, usersPhotos) => {
+    openBigPicture();
+    const bigPictureId = +photo.id;
+    const { comments, description } = usersPhotos.find((item) => item.id === bigPictureId);
+    comments.forEach(({ avatar, name, message }) => {
+      const commentElement = commentTemplate.cloneNode(true);
+      commentElement.querySelector('.social__picture').src = avatar;
+      commentElement.querySelector('.social__picture').alt = name;
+      commentElement.querySelector('.social__text').textContent = message;
+      commentsList.append(commentElement);
+    });
+    const bigPictureDescription = modalBigPicture.querySelector('.social__caption');
+    const bigPictureUrl = modalBigPicture.querySelector('.big-picture__img > img');
+    const bigPictureLikes = modalBigPicture.querySelector('.likes-count');
+    const bigPictureCommentsCount = modalBigPicture.querySelector('.comments-count');
+    bigPictureUrl.src = photo.querySelector('.picture__img').src;
+    bigPictureLikes.textContent = photo.querySelector('.picture__likes').textContent;
+    bigPictureCommentsCount.textContent = photo.querySelector('.picture__comments').textContent;
+    bigPictureDescription.textContent = description;
+  };
+  for (const userPhoto of otherUsersPhotos) {
+    userPhoto.addEventListener('click', () => addPhotoContent(userPhoto, similarPhotos));
+  }
 };
 
-const addPhotoContent = (photo) => {
-  openBigPicture();
-  const bigPictureUrl = modalBigPicture.querySelector('.big-picture__img').querySelector('img');
-  const bigPictureLikes = modalBigPicture.querySelector('.likes-count');
-  const bigPictureCommentsCount = modalBigPicture.querySelector('.comments-count');
-  bigPictureUrl.src = photo.querySelector('.picture__img').src;
-  bigPictureLikes.textContent = photo.querySelector('.picture__likes').textContent;
-  bigPictureCommentsCount.textContent = photo.querySelector('.picture__comments').textContent;
-};
-
-for (const userPhoto of otherUsersPhotos) {
-  userPhoto.addEventListener('click', () => addPhotoContent(userPhoto));
-}
-
-
+export { showBigPicture };
