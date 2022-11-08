c1b1c90c51106f4170159efaff78be924572cb58
FrenzyPet <manpatbro@gmail.com>
2022-11-08 16:36:21 +0300

    импорт переименованного модуля

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { renderOtherUsersPhoto, similarPhotos } from './render-thumbnails.js';
 import { showBigPicture } from './show-big-picture.js';
-import './sumbit-form.js';
+import './form.js';
 
 renderOtherUsersPhoto();
 showBigPicture(similarPhotos);
